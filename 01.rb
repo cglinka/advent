eaf24185f3fe425500a0f2147abcac56b0f6c76d
Clare Glinka <clare.glinka@returnpath.com>
2016-01-06 16:51:48 -0600

    first challenge, part 2

--- 01.rb
+++ 01.rb
@@ -2,12 +2,18 @@ floor = 0
 
 file = File.read("01.txt")
 
-file.each_char do |c|
+file.each_char.with_index do |c, index|
   if c == "("
     floor += 1
   elsif c == ")"
     floor -= 1
   end
+
+  if floor == -1
+    # Should be 1783
+    puts "Enter basement at #{index + 1}"
+  end
 end
 
-puts floor
+# Should be 232
+puts "Final floor position: #{floor}"
