24ff779cfa0cef11d904cdb7b6025e4547129102
Clare Glinka <clare.glinka@returnpath.com>
2018-08-13 12:40:06 -0500

    day 2

--- 2015/02.rb
+++ 2015/02.rb
@@ -2,6 +2,14 @@ paper_order = 0
 
 file = File.read("02.txt")
 
+paper = 0
+ribbon = 0
 file.each_line.with_index do |line, index|
-    
+    # print line
+    l = line.rstrip.split("x").map(&:to_i)
+    l.sort!
+    paper += (l[0]*l[1]*2) + (l[0]*l[2]*2 + l[1]*l[2]*2) + l[0]*l[1]
+    ribbon += l[0]*2 + l[1]*2 + l[0]*l[1]*l[2]
 end
+puts "paper: #{paper}"
+puts "ribbon: #{ribbon}"
